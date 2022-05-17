65528052ef774a6def39914c95852f7026613456
Felipe Huffner <felipeqh.1991@gmail.com>
2022-05-17 15:46:44 -0400

    Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
-
+import styles from './home.module.scss';
 import Head from "next/head";
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Home IgNews</title>
+        <title>Home | Ig.news</title>
       </Head>
-      <h1>
-        Hello <span>World</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+              News about the <span>React</span> world
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.99 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="girl Coding" />
+      </main>
     </>
   );
 }
