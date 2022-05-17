6d249a788a490a75f2103d46c04626ab870e53c9
Felipe Huffner <felipeqh.1991@gmail.com>
2022-05-17 17:27:30 -0400

    Consumindo Stripe via SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import { GetServerSideProps } from 'next';
 import styles from './home.module.scss';
 import Head from "next/head";
 import { SubscribeButton } from '../components/SubscribeButton';
-export default function Home() {
+import { stripe } from '../services/stripe';
+interface HomeProps {
+  product:{
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -15,12 +24,28 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.99 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="girl Coding" />
       </main>
     </>
   );
+}
+export const getServerSideProps: GetServerSideProps = async () =>{
+    const price = await stripe.prices.retrieve('price_1L0WrfFWsfqH44occWoJStBx',{
+})
+    const product={
+      priceId: price.id,
+      amount: new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+    return{
+       props:{
+         product,
+       }
+      }
 }
