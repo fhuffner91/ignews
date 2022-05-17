fffbb8701a61eece01338ba8e912c9efe9494669
Felipe Huffner <felipeqh.1991@gmail.com>
2022-05-17 16:00:31 -0400

    subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from './home.module.scss';
 import Head from "next/head";
+import { SubscribeButton } from '../components/SubscribeButton';
 export default function Home() {
   return (
     <>
@@ -16,6 +17,7 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.99 month</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="girl Coding" />
       </main>
