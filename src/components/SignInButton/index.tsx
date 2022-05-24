007a043e7dbc997ff1920814b50cdd8d010328cc
Felipe Huffner <felipeqh.1991@gmail.com>
2022-05-24 13:02:47 -0400

    NextAuth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,20 @@
 import {FaGithub} from 'react-icons/fa';
 import styles from './styles.module.scss';
 import {FiX} from 'react-icons/fi'
+import {signIn, signOut, useSession } from 'next-auth/react';
 export function SignInButton() {
-    const isUserLoggedIn = true;
-  return isUserLoggedIn ?(
+    const {data:session}=useSession();
+
+    console.log(session);
+
+  return session ?(
       <button
       type='button'
       className={styles.signInButton}
+      onClick={()=>signOut()}
       >
           <FaGithub color='#04d361'/>
-          Felipe Huffner
+          {session.user.name}
           <FiX color='#737380' className={styles.closeIcon}/>
       </button>
   ):
@@ -17,6 +22,7 @@ export function SignInButton() {
     <button
     type='button'
     className={styles.signInButton}
+    onClick={() => signIn('github')}
     >
         <FaGithub color='#eba417'/>
         Sign In with Github
